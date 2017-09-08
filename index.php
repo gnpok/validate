c6519eb04c7f520df88cb40f71e740453af09f1a
apple <gnpok@qq.com>
2017-09-08 14:23:55 +0800

    添加代码程序

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
-# validate
-Validate input data
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2017/9/8
+ * Time: 上午10:55
+ */
 
-> 提取出了tp5的规则验证
 
-### 注意事项:
-- 1 require 已变为required
-- 2 number 更改为 integer
-- 3 unique 被注释掉了
-
-### 使用方法:
-```php
 require_once dirname(__FILE__).'/vendor/autoload.php';
 use tp5\validate\Validate;
 
 $validate = new Validate([
-    'name' => 'required|max:3',
+    'name' => 'require|max:3',
     'email' => 'require',
     'url' => 'activeUrl'
 ]);
@@ -31,7 +28,3 @@ if($validate->check($data)){
     var_dump($validate->getError());
 }
 
-```
-
-### 存在问题:
-> 独立出来后，多多少少会出现问题，我也会在将来的实践中继续修改
